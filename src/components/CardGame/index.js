cb9575ddc324a12880eaa736e5e7ee6b8a6d1eb7
Thiago Martins <thiagoamartins0@gmail.com>
2022-06-30 15:43:43 -0300

    :tada: Create CardGame component

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,11 @@
+import "./style.css";
+
+function CardGame() {
+    return `
+        <article class="card-game">
+            <img src="./src/images/alura-pixel.png" alt="Logo da Alura">
+        </article>
+    `;
+}
+
+export default CardGame;
