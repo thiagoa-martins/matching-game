3b991332addc8414223b38b948f75c18c5a46b78
Thiago Martins <thiagoamartins0@gmail.com>
2022-07-06 15:05:41 -0300

    :construction: Add CardFrontBack in BoardGame

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
 import "./src/styles/settings/colors.css";
 import "./src/styles/generic/reset.css"
 import "./src/styles/elements/base.css";
-import CardGameVerse from "./src/components/CardGameVerse";
-import BoardGame from "./src/objects/BoardGame";
 import ScoreBoard from "./src/objects/ScoreBoard";
+import BoardGame from "./src/objects/BoardGame";
 
 const $root = document.querySelector("#root");
 
 $root.insertAdjacentHTML(
     "beforeend",
     `   ${ScoreBoard()}
-        ${BoardGame(6)}
+        ${BoardGame(2)}
     `
 );
 
