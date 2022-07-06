a80a171eff97781c46040e475c687961c38b734c
Thiago Martins <thiagoamartins0@gmail.com>
2022-07-06 12:13:55 -0300

    :art: Add html comment in template string

--- src/components/CardGameVerse/index.js
+++ src/components/CardGameVerse/index.js
@@ -1,7 +1,7 @@
 import "./style.css";
 
 function CardGameVerse(nameImage, alt) {
-    return `
+    return /*html*/`
         <article class="card-game-verse">
             <img src="./src/images/${nameImage}" alt="${alt}">
         </article>
--- src/components/PlayerName/index.js
+++ src/components/PlayerName/index.js
@@ -1,7 +1,7 @@
 import "./style.css";
 
 function PlayerName(player) {
-    return `
+    return /*html*/`
         <p class="player-name">${player}</p>
     `;
 }
