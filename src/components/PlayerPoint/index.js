58c428d042acfb393b2ed9692a71a9f0b306ef78
Thiago Martins <thiagoamartins0@gmail.com>
2022-07-08 20:34:16 -0300

    :sparkles: Create PlayerPoint and PlayerVs components

---
+++ src/components/PlayerPoint/index.js
@@ -0,0 +1,13 @@
+import "./style.css";
+
+function PlayerPoint() {
+    return /*html*/`
+        <article class="player-point">
+            <div></div>
+            <div></div>
+            <div></div>
+        </article>
+    `;
+}
+
+export default PlayerPoint;
---
+++ src/components/PlayerVs/index.js
@@ -0,0 +1,9 @@
+import "./style.css";
+
+function PlayerVs() {
+    return /*html*/`
+        <article class="player-vs">vs</article>
+    `;
+}
+
+export default PlayerVs;
