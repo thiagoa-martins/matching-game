c58809f5af946ebce356967a2a6dbc7ef18a7474
Thiago Martins <thiagoamartins0@gmail.com>
2022-07-08 20:38:06 -0300

    :art: Add PlayerPoint and PlayerVs on ScoreBoard

--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -1,10 +1,15 @@
 import "./style.css";
 import PlayerName from "../../components/PlayerName";
+import PlayerVs from "../../components/PlayerVs";
+import PlayerPoint from "../../components/PlayerPoint";
 
 function ScoreBoard() {
     return /*html*/ `
         <header class="score-board">
             ${PlayerName("Player1")}
+            ${PlayerPoint()}
+            ${PlayerVs()}
+            ${PlayerPoint()}
             ${PlayerName("Player2")}
         </header>
     `; 
