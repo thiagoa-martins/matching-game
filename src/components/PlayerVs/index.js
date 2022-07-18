8ac88570d3a66aa5943a775a0b5610178705ba33
Thiago Martins <thiagoamartins0@gmail.com>
2022-07-18 11:48:56 -0300

    :construction: Rename VsPlayer component

--- src/components/PlayerVs/index.js
+++
@@ -1,9 +0,0 @@
-import "./style.css";
-
-function PlayerVs() {
-    return /*html*/`
-        <article class="player-vs">vs</article>
-    `;
-}
-
-export default PlayerVs;
--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -1,14 +1,14 @@
 import "./style.css";
 import PlayerName from "../../components/PlayerName";
-import PlayerVs from "../../components/PlayerVs";
 import PlayerPoint from "../../components/PlayerPoint";
+import VsPlayer from "../../components/VsPlayer";
 
 function ScoreBoard() {
     return /*html*/ `
         <header class="score-board">
             ${PlayerName("Player1")}
             ${PlayerPoint()}
-            ${PlayerVs()}
+            ${VsPlayer()}
             ${PlayerPoint()}
             ${PlayerName("Player2")}
         </header>
