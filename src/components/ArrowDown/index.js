b3f7278176f305d2f510b47d0c637d2aab5288df
Thiago Martins <thiagoamartins0@gmail.com>
2022-07-18 16:31:51 -0300

    :lipstick: Add CSS in ArrowDown with animation

--- src/components/ArrowDown/index.js
+++ src/components/ArrowDown/index.js
@@ -1,8 +1,11 @@
 import "./style.css";
 
-function ArrowDown() {
+function ArrowDown(currentPlayer = 1) {
     return /*html*/`
-        <img class="arrow-down" src="./src/images/icon-arrow-down.png" alt="Arrow down">
+        <img class="arrow-down" 
+             data-currentPlayer="${currentPlayer}"
+             src="./src/images/icon-arrow-down.png" 
+             alt="Arrow down">
     `;
 }
 
