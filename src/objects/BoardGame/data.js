bdd7dc6124f2768478e39945fcedfffe30028ed9
Thiago Martins <thiagoamartins0@gmail.com>
2022-07-19 11:48:36 -0300

    :sparkles: Create the javascript, html and css cards

---
+++ src/objects/BoardGame/data.js
@@ -0,0 +1,26 @@
+const cards = [{
+    icon: "logo-css",
+    altIcon: "Logo do Css"
+},
+{
+    icon: "logo-css",
+    altIcon: "Logo do Css"
+},
+{
+    icon: "logo-html",
+    altIcon: "Logo do Html"
+},
+{
+    icon: "logo-html",
+    altIcon: "Logo do Html"
+},
+{
+    icon: "logo-javascript",
+    altIcon: "Logo do Javascript"
+},
+{
+    icon: "logo-javascript",
+    altIcon: "Logo do Javascript"
+}];
+
+export default cards;
